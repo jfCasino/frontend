8e207bf16186b429b2fde814213b867d6575bfc8
Jaka Furlan <jaka.furlan@cosylab.com>
2026-01-06 21:46:06 +0100

    prod ready????

--- src/app/seznami/wallet.component.ts
+++ src/app/seznami/wallet.component.ts
@@ -6,7 +6,6 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-wallet',
   templateUrl: './wallet.component.html',
-  styleUrls: ['./wallet.component.css']
 })
 export class WalletComponent implements OnInit {
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://nakupovanje.prpo.si/v1'
+  production: true,
+  apiUrl: 'https://jfcasino.com/api',                 // base API URL
+  rouletteServiceUrl: 'https://jfcasino.com/roulette', // roulette service endpoint
+  statsServiceUrl: 'https://jfcasino.com/stats',       // stats service endpoint
+  walletServiceUrl: 'https://jfcasino.com/wallets'    // wallet service endpoint
 };
